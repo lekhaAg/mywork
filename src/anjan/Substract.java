deeda801b1116508c7b24f8796b7f7f7b074de5e
lekha <lekha@pcomputer>
2019-03-01 15:20:08 +0530

    new subtract items

---
+++ src/anjan/Substract.java
@@ -0,0 +1,15 @@
+package anjan;
+
+public class Substract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=50;
+		b=20;
+		c=50-20;
+		System.out.println(c);
+
+	}
+
+}
