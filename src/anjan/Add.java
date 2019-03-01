870eca19284cb1597d0ca37ff7b0167fe1aaf746
lekha <lekha@pcomputer>
2019-03-01 14:38:47 +0530

    new generated code

---
+++ src/anjan/Add.java
@@ -0,0 +1,17 @@
+package anjan;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c;
+		c=a+b;
+		System.out.println(c);
+		System.out.println("i loveu "
+				+ "c");
+
+	}
+
+}
